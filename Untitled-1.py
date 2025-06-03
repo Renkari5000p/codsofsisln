bae0d9f146a5ebd1b31f98733826234ee3c4beb1
Renkari5000p <gallardo.carlos.2024@cbtis-246.edu.mx>
2025-06-02 18:55:29 -0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,16 @@
+def python_snake(xs):
+    n = xs
+    head = "H"
+    body = "X"
+    tail = "T"
+    longitud = len(xs)
+    columnas = max(xs)
+    matrix=[]
+    for i in range(longitud):
+        row = []
+        for x in range(columnas):
+            row.append('H')
+        if 
+        
+        matrix.append(row)
+    return matrix
