1ccfd471a1cf5c598d3e384ffa1b931f733e765e
Renkari5000p <gallardo.carlos.2024@cbtis-246.edu.mx>
2025-06-16 18:02:36 -0600

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,2 @@
-def calculate(s):
-    return str(eval(s.replace("plus", "+").replace("minus", "-")))
+def decodeMorse(morseCode):
+    return ' '.join(''.join(MORSE_CODE[letter] for letter in word.split(' ')) for word in morseCode.strip().split('   '))
